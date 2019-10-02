9ee2ac67925ff0b09df06022f86a9c42a23483f3
soulone <ascf24032@gmail.com>
2019-10-02 10:38:13 -0500

    Inicializando el proyecto
    Inicializando el proyecto de SoundBoard

---
+++ SoundBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundBoard
+//
+//  Created by Antony Sebastian Cueva Fajardo on 10/2/19.
+//  Copyright © 2019 Antony Sebastian Cueva Fajardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
